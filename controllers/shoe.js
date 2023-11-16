f6d069eb22e727c22909748c3b0f88ae88106e0a
Venkateshkoya <venkateshkoya8@gmail.com>
2023-11-15 18:25:48 -0600

    updated with create

--- controllers/shoe.js
+++ controllers/shoe.js
@@ -103,3 +103,14 @@ exports.shoe_view_one_Page = async function (req, res) {
         res.send(`{'error': '${err}'}`);
     }
 };
+
+exports.shoe_create_Page = function (req, res) {
+    console.log("create view")
+    try {
+        res.render('shoecreate', { title: 'shoe Create' });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
--- routes/shoe.js
+++ routes/shoe.js
@@ -4,6 +4,7 @@ var router = express.Router();
 /* GET shoes */
 router.get('/', shoe_controllers.shoe_view_all_Page );
 router.get('/detail', shoe_controllers.shoe_view_one_Page);
+router.get('/create', shoe_controllers.shoe_create_Page);
 module.exports = router;
 
 /* GET detail costume page */
