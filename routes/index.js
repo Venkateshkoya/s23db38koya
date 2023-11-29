f8c23621c7c773a3007ee33208c7eb2a2d00b6f4
Venkateshkoya <venkateshkoya8@gmail.com>
2023-11-29 10:13:31 -0600

    changes made

--- routes/index.js
+++ routes/index.js
@@ -14,65 +14,73 @@ var router = express.Router();
 var Account = require('../models/account');
 
 router.get('/', function (req, res) {
- res.render('index', { title: 'Shoe App', user : req.user });
+  res.render('index', { title: 'Shoe App', user: req.user });
 });
 
-router.get('/register', function(req, res) {
- res.render('register', { title: 'Shoe App Registration'});
+router.get('/register', function (req, res) {
+  res.render('register', { title: 'Shoe App Registration' });
 });
 
-router.post('/register', function(req, res) {
- Account.findOne({ username : req.body.username })
- .then(function (user){
- if(user != null ){
- console.log("exists " + req.body.username)
- return res.render('register', { title: 'Registration', 
- message: 'Existing User', account : req.body.username })
- }
- let newAccount = new Account({ username : req.body.username });
- Account.register(newAccount, req.body.password, function(err, user){
- if (err) {
- console.log("db creation issue "+ err)
- return res.render('register', { title: 'Registration', 
- message: 'access error', account : req.body.username })
- }
- if(!user){
- return res.render('register',{ title: 'Registration', 
- message: 'access error', account : req.body.username })
- }
- })
- console.log('Sucess, redirect');
- res.redirect('/');
- })
- .catch(function (err){
- return res.render('register', { title: 'Registration', 
- message: 'Registration error', account : req.body.username })
- })
+router.post('/register', function (req, res) {
+  Account.findOne({ username: req.body.username })
+    .then(function (user) {
+      if (user != null) {
+        console.log("exists " + req.body.username)
+        return res.render('register', {
+          title: 'Registration',
+          message: 'Existing User', account: req.body.username
+        })
+      }
+      let newAccount = new Account({ username: req.body.username });
+      Account.register(newAccount, req.body.password, function (err, user) {
+        if (err) {
+          console.log("db creation issue " + err)
+          return res.render('register', {
+            title: 'Registration',
+            message: 'access error', account: req.body.username
+          })
+        }
+        if (!user) {
+          return res.render('register', {
+            title: 'Registration',
+            message: 'access error', account: req.body.username
+          })
+        }
+      })
+      console.log('Sucess, redirect');
+      res.redirect('/');
+    })
+    .catch(function (err) {
+      return res.render('register', {
+        title: 'Registration',
+        message: 'Registration error', account: req.body.username
+      })
+    })
 });
 
 
-router.get('/login', function(req, res) {
-  res.render('login', { title: 'Shoe App Login', user : req.user });
- });
+router.get('/login', function (req, res) {
+  res.render('login', { title: 'Shoe App Login', user: req.user });
+});
 
- router.post('/login', passport.authenticate('local'), function(req, res) {
+router.post('/login', passport.authenticate('local'), function (req, res) {
   res.redirect('/');
- });
+});
 
- router.get('/logout', function(req, res) {
-  req.logout(function(err) {
-  if (err) { return next(err); }
-  res.redirect('/');
+router.get('/logout', function (req, res) {
+  req.logout(function (err) {
+    if (err) { return next(err); }
+    res.redirect('/');
   });
- });
+});
 
- router.get('/ping', function(req, res){
+router.get('/ping', function (req, res) {
   res.status(200).send("pong!");
- });
+});
 
- module.exports = router;
- router.get('/ping', function(req, res){
+module.exports = router;
+router.get('/ping', function (req, res) {
   res.status(200).send("pong!");
- });
- 
- module.exports = router;
+});
+
+module.exports = router;
