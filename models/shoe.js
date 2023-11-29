1f4025f75343e9bfe208c29f10cc1c2f139a671f
Venkateshkoya <venkateshkoya8@gmail.com>
2023-11-29 10:11:04 -0600

    added min, max

--- models/shoe.js
+++ models/shoe.js
@@ -1,7 +1,14 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+
 const shoeSchema = mongoose.Schema({
-brand: String,
-color: String,
-size: Number
-})
-module.exports = mongoose.model("shoe", shoeSchema)
+  brand: String,
+  color: String,
+  size: {
+    type: Number,
+    min: [5, "Size must be at least 5"],
+    max: [20, "Size cannot be greater than 20"],
+  
+  }
+});
+
+module.exports = mongoose.model("shoe", shoeSchema);
